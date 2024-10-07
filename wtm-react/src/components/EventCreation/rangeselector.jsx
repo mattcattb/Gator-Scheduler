e31ae61fa3ea42a00370cf1a5864453a33b31b59
mattcattb <mboughton17@gmail.com>
2024-10-07 11:35:21 -0400

    added selected days use state

--- wtm-react/src/components/EventCreation/rangeselector.jsx
+++ wtm-react/src/components/EventCreation/rangeselector.jsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import { Typography, Box, Checkbox, FormControlLabel } from '@mui/material';
 
 // Define days of the week and time range
-const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const startHour = 9; // 9:00 AM
 const endHour = 17; // 5:00 PM
 const timeSlots = generateTimeSlots(startHour, endHour, 30); // 30-minute intervals
+
+/*
+  todo:
+  - Remove unselected days from grid and readjust
+  - add a help section to explain how to use the grid
+  - make simpler? More Appealing?
+  - Add a section to adjust time range only
+  - make sections more responsive
+
+*/
 
 // Function to generate time slots
 function generateTimeSlots(start, end, interval) {
@@ -22,14 +32,25 @@ function generateTimeSlots(start, end, interval) {
 }
 
 function PossibilityRangeSelector() {
+  const [selectedDays, setSelectedDays] = useState({});
+
+  const toggleDaySelection = (day) => {
+    setSelectedDays((prev) => ({
+      ...prev,
+      [day]: !prev[day],
+    }));
+  };
+
   return (
     <div>
-      <TimeSelectorGrid/>
+      <DaySelection selectedDays={selectedDays} toggleDaySelection={toggleDaySelection} />
+      <TimeSelectorGrid selectedDays={selectedDays} />
     </div>
   );
 }
 
-function TimeSelectorGrid() {
+function TimeSelectorGrid({ selectedDays }) {
+  // Manage the Times selected for each day
   const [selectedTimes, setSelectedTimes] = useState({});
 
   const toggleTimeSlot = (day, time) => {
@@ -68,30 +89,65 @@ function TimeSelectorGrid() {
         {timeSlots.map((time) => (
           <React.Fragment key={time}>
             {daysOfWeek.map((day) => (
-              <FormControlLabel
+              <Box
                 key={`${day}-${time}`}
-                control={
-                  <Checkbox
-                    checked={selectedTimes[day]?.[time] || false}
-                    onChange={() => toggleTimeSlot(day, time)}
-                    sx={{
-                      color: selectedTimes[day]?.[time] ? '#0021A5' : '#FA4616', // Highlighted or unhighlighted
-                      '&.Mui-checked': { color: '#0021A5' }, // Use Gator blue for selected
-                    }}
-                  />
-                }
-                label={time}
                 sx={{
-                  backgroundColor: selectedTimes[day]?.[time] ? '#0021A5' : '#FA4616', // Gator blue or orange
-                  color: '#fff',
-                  padding: 1,
-                  borderRadius: 1,
-                  cursor: 'pointer',
-                  textAlign: 'center', // Center the text
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  opacity: selectedDays[day] ? 1 : 0.5, // Change opacity if day is not selected
                 }}
-              />
+              >
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={selectedTimes[day]?.[time] || false}
+                      onChange={() => toggleTimeSlot(day, time)}
+                      sx={{
+                        color: selectedTimes[day]?.[time] ? '#0021A5' : '#FA4616',
+                        '&.Mui-checked': { color: '#0021A5' },
+                      }}
+                    />
+                  }
+                  label={time}
+                  sx={{
+                    backgroundColor: selectedTimes[day]?.[time] ? '#0021A5' : '#FA4616',
+                    color: '#fff',
+                    padding: 1,
+                    borderRadius: 1,
+                    cursor: 'pointer',
+                    textAlign: 'center',
+                  }}
+                />
+              </Box>
             ))}
           </React.Fragment>
+        ))}
+      </Box>
+    </Box>
+  );
+}
+
+
+function DaySelection({ selectedDays, toggleDaySelection }) {
+  // manage the Days Selected
+  return (
+    <Box sx={{ padding: 3 }}>
+      <Typography variant="h4" gutterBottom>
+        Select Days
+      </Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        {daysOfWeek.map((day) => (
+          <FormControlLabel
+            key={day}
+            control={
+              <Checkbox
+                checked={selectedDays[day] || false}
+                onChange={() => toggleDaySelection(day)}
+              />
+            }
+            label={day}
+          />
         ))}
       </Box>
     </Box>
