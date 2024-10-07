15b673a8af5ffb7483f52b43202f032a0aca27c9
mattcattb <mboughton17@gmail.com>
2024-10-07 02:24:23 -0400

    began working on seperate event creator component for event creator page

---
+++ wtm-react/src/components/EventCreation/eventcreator.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './eventcreator.css';
+
+import AddFriends from './addfriends';
+import DaySelection from './dayselection';
+import EventHeader from './eventheader';
+
+import { useState } from 'react';
+
+function EventCreator() {
+    const [eventName, setEventName] = useState('');  // State for event name
+    const [eventDescription, setEventDescription] = useState('');  // State for event description
+    // selected days is an array of true or false values, where each index represents a day of the week
+    // const [selectedDays, setSelectedDays] = useState([false, false, false, false, false, false, false]);
+    return (
+      <div className="event-creation-container">
+        
+        <EventHeader 
+          eventName={eventName} 
+          setEventName={setEventName} 
+          eventDescription={eventDescription} 
+          setEventDescription={setEventDescription}
+        />
+        <DaySelection /> 
+        <AddFriends />
+  
+        <div className="section">
+          <button className="submit-btn">Create Event</button>
+        </div>
+      </div>
+    );
+  }
+    
+export default EventCreator;
