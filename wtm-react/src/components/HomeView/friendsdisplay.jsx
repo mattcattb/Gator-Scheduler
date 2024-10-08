6e9c58d05602f0f7ab369640768bc27e0481d18b
mattcattb <mboughton17@gmail.com>
2024-10-08 00:58:08 -0400

    Bagan homeapge header and Friends Display

--- wtm-react/src/components/HomeView/friendsdisplay.jsx
+++ wtm-react/src/components/HomeView/friendsdisplay.jsx
@@ -1,7 +1,11 @@
+import { Box, Typography, Card, CardContent, Button } from '@mui/material';
+
 function FriendsDisplay() {
     // show all of your friends, when clicked view their schedule
     return (
-      <></>
+      <Box>
+        <Typography variant='h4' gutterBottom>Your Friends</Typography>
+      </Box>
     )
   }
 
