5845e3605e1f358c7332e262093e3c9bed891e17
mattcattb <mboughton17@gmail.com>
2024-10-16 16:38:46 -0400

    added fake link to join or invite people

--- wtm-react/src/components/EventCreation/addfriends.jsx
+++ wtm-react/src/components/EventCreation/addfriends.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 
 function AddFriends() {
   return (
-    <div className="section">
-        <h2>Add Friends</h2>
-        <select className="friend-selector">
-            <option value="">Select Friends</option>
-            {/* Static options for now */}
-            <option value="friend1">Friend 1</option>
-            <option value="friend2">Friend 2</option>
-        </select>
-    </div>
+    <>
+      <div className="section">
+          <h2>Add Friends</h2>
+          <select className="friend-selector">
+              <option value="">Select Friends</option>
+              {/* Static options for now */}
+              <option value="friend1">Friend 1</option>
+              <option value="friend2">Friend 2</option>
+          </select>
+      </div>
+      <div className="section"> 
+        <a href="https://your-link.com">Copy and paste to join!</a>
+      </div>
+    </>
   )
 }
 
