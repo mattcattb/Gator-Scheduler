cf76c35f0c14adcdaaa2dbabf996e6993d7c83ae
mattcattb <mboughton17@gmail.com>
2024-10-16 16:39:30 -0400

    Added a Schedule Display to the calender

---
+++ wtm-react/src/components/HomeView/scheduledisplay.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Box, Typography, Button} from '@mui/material';
+
+export default function ScheduleDisplay() {
+  return (
+    <Box>
+        <Typography variant='h4' gutterBottom>Your Schedule</Typography>
+        <Button variant='contained'>Edit</Button>
+        <SchedulePreview/>
+    </Box>
+  )
+}
+
+
+function SchedulePreview() {
+    // preview of your schedule
+
+}
--- wtm-react/src/pages/homeview.jsx
+++ wtm-react/src/pages/homeview.jsx
@@ -1,12 +1,14 @@
 import JoinedEvents from "../components/HomeView/joinedevents";
 import HomepageHeader from "../components/HomeView/homepageheader";
 import FriendsDisplay from "../components/HomeView/friendsdisplay";
+import ScheduleDisplay from "../components/HomeView/scheduledisplay";
 
 function HomeView() {
     return (
       <div className='HomeView'>
         <HomepageHeader />
         <JoinedEvents events={[]}/>
+        <ScheduleDisplay />
         <FriendsDisplay />
       </div>
     );
