9fd7e069097a6401f65a890db2bb891b6f690449
mattcattb <mboughton17@gmail.com>
2024-10-17 16:49:02 -0400

    YAYYY restored stuff

--- wtm-react/src/pages/homeview.jsx
+++ wtm-react/src/pages/homeview.jsx
@@ -1,15 +1,23 @@
 import JoinedEvents from "../components/HomeView/joinedevents";
-import HomepageHeader from "../components/HomeView/homepageheader";
 import FriendsDisplay from "../components/HomeView/friendsdisplay";
+import ScheduleDisplay from "../components/HomeView/scheduledisplay.jsx";
 
-function HomeView() {
+import { Typography } from "@mui/material";
+const sampleFriends = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' },
+  { id: 4, name: 'Diana' },
+];
+const sampleUser = "Matty_User"
+
+export default function HomeView() {
     return (
-      <div className='HomeView'>
-        <HomepageHeader />
+      <div className='HomeView' style={{ padding: '20px', maxWidth: '1200px', margin: 'auto' }}>
+        <Typography variant='h2' gutterBottom>Welcome, {sampleUser}!</Typography>
         <JoinedEvents events={[]}/>
-        <FriendsDisplay />
+        <ScheduleDisplay />
+        <FriendsDisplay friends={sampleFriends}/>
       </div>
     );
   }
-  
-  export default HomeView;
