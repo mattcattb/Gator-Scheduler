c76afa9a48836eb58151dc8ed5192908c9ac270b
Corey Phillips <coreypofficial@gmail.com>
2024-10-24 09:36:28 -0400

    updated store to fix profile sync bug, added user icons, and made basic layout of user display

--- wtm-react/src/context/UserProvider.jsx
+++ wtm-react/src/context/UserProvider.jsx
@@ -1,12 +1,23 @@
 // context/UserContext.js
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 
 // Create the context
 export const UserContext = createContext();
 
 // Create the provider component
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Initially, user is null
+  const [user, setUser] = useState(()=>{
+    const savedUser = sessionStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  }); // Initially, user is null if the user does not exist in the temporary storage
+
+  useEffect(() => {
+    if (user) {
+      sessionStorage.setItem('user', JSON.stringify(user));
+    } else {
+      sessionStorage.removeItem('user');
+    }
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
--- wtm-react/src/pages/profile.jsx
+++ wtm-react/src/pages/profile.jsx
@@ -1,12 +1,32 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../context/UserProvider';
 
+import "../components/Profile/profile.css"
+
+function iconIdToPath(id){
+  const map = new Map([
+    ["1", "/user_icons/user_icon_1.png"],
+    ["2", "/user_icons/user_icon_2.png"],
+    ["3", "/user_icons/user_icon_3.png"],
+    ["4", "/user_icons/user_icon_4.png"],
+    ["5", "/user_icons/user_icon_5.png"],
+  ])
+  return map.get(id);
+}
+
 function Profile() {
   const { user } = useContext(UserContext);
 
     return (
-      <div className="Profile">
-        Profile Page {user.username}
+      <div className="profile-container">
+        <div className="user-info">
+          <img src={iconIdToPath(user.icon)} alt="User Icon" 
+          className="user-icon" />
+          <div className='user-details'>
+            <h2 className="username">{user.username}</h2>
+            <p className="friend-code">Friend Code: {user._id}</p>
+          </div>
+        </div>
       </div>
     );
   }
