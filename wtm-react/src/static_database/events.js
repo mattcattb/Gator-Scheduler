986267143534dc223b0cdfdc6d9e3ec150e6630c
mattcattb <mboughton17@gmail.com>
2024-11-05 16:46:44 -0500

    added more todo and added description to events in static database

--- wtm-react/src/static_database/events.js
+++ wtm-react/src/static_database/events.js
@@ -2,24 +2,29 @@ const example_events = [
     {
       id: '1',
       title: 'Event 1',
+      description: 'fdfd',
       start: '2024-10-16 09:00', // Start time
       end: '2024-10-16 10:00',   // End time
     },
     {
       id: '2',
       title: 'Event 2',
+      description: 'fdfd',
       start: '2024-10-17 13:00', // Start time
       end: '2024-10-17 14:30',   // End time
     },
     {
       id: '3',
       title: 'Event 3',
+      description: 'fdfd',
       start: '2024-10-18 11:00', // Start time
       end: '2024-10-18 12:00',   // End time
     },
     {
       id: '4',
       title: 'Event 4',
+      description: 'fdfd',
+
       start: '2024-10-19 15:30', // Start time
       end: '2024-10-19 17:00',   // End time
     },
