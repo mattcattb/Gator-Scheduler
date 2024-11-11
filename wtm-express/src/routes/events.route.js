1527f14a680f0d6c1891166e7c95ef556948e01f
Matthew Boughton <55923494+mattcattb@users.noreply.github.com>
2024-11-10 22:57:15 -0500

    fixed routes in the router for events route

--- wtm-express/src/routes/events.route.js
+++ wtm-express/src/routes/events.route.js
@@ -1,16 +1,14 @@
 const { Router } = require('express');
 const router = Router();
 const User = require('../models/user.js');
-const Event = require('../models/user.js');
+const Event = require('../models/event.js');
 
 import { getEvents, postEvent, editEvent, deleteEvent } from "../controllers/events.controller.js"
 
 // Returns list of events in JSON for front end to display
-router.get('/getEvents', getEvents);
-router.post('/postEvent', postEvent);
-router.put('/editEvent', editEvent);
-router.delete('/deleteEvent', deleteEvent);
-
-
+router.get('/', getEvents);
+router.post('/', postEvent);
+router.put('/', editEvent);
+router.delete('/', deleteEvent);
 
 module.exports = router;
