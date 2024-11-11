874d27c0f53085f1d40496568b9cfe037446a74a
Matthew Boughton <55923494+mattcattb@users.noreply.github.com>
2024-11-10 22:56:58 -0500

    created a postevent, edit event and delete event functions that need to be completed

--- wtm-express/src/controllers/events.controller.js
+++ wtm-express/src/controllers/events.controller.js
@@ -1,18 +1,55 @@
 import Event from "../models/event.js";
+import User from "../models/user.js";  // Assuming you have a User model
+
 
 export const getEvents = async (req, res) => {
     try {
-        const { userId } = req.body;
+        const { userId } = req.query;  // Use req.query for query parameters
         const user = await User.findById(userId);
          
         if (!user) return res.status(204).json({ message: 'User not found' });
  
         res.json(user.events);
+        
  
     } catch (err) {
+        console.error(err);
         res.status(500).send('Server error');
     }
 }
 
-postEvent, editEvent, deleteEvent
+export const postEvent = async (req, res) => {
+    try {
+        const { userId, title, description, start, end } = req.body;
+        
+        if(!userId || !title || !description || !start || !end){
+            return res.status(400).json({message:'Field information missing.'});
+        }
+
+        const newEvent = new Event({title, description, start, end});
+        await newEvent.save();
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({message: "User not found."});
+        }
+        user.events.push(newEvent._id); // find the user and then push this _id of the event to it
+        await user.save();
+        
+        res.status(201).json(newEvent); // return this new event
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+        
+    }
+}
+
+export const editEvent = async(req, res) => {
+
+}
+
+export const deleteEvent = async(req, res) => {
+
+}
 
