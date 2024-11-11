c0e126d5e7f575156dc1ab6640e1baaa22e2ff51
Matthew Boughton <55923494+mattcattb@users.noreply.github.com>
2024-11-10 23:08:32 -0500

    added params to routing

--- wtm-express/src/routes/events.route.js
+++ wtm-express/src/routes/events.route.js
@@ -1,14 +1,12 @@
 const { Router } = require('express');
 const router = Router();
-const User = require('../models/user.js');
-const Event = require('../models/event.js');
 
-import { getEvents, postEvent, editEvent, deleteEvent } from "../controllers/events.controller.js"
+const { getEvents, postEvent, editEvent, deleteEvent } = require("../controllers/events.controller.js");
 
 // Returns list of events in JSON for front end to display
 router.get('/', getEvents);
 router.post('/', postEvent);
-router.put('/', editEvent);
-router.delete('/', deleteEvent);
+router.put('/:eventId', editEvent);
+router.delete('/:eventId', deleteEvent);
 
 module.exports = router;
