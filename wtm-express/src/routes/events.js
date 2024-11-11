7a24afd8a074546aa54eaa85c35bca144d2188b6
Charles Durkin <cdurkin35@gmail.com>
2024-11-10 20:33:24 -0500

    Create getEvent route

---
+++ wtm-express/src/routes/events.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const router = Router();
+const User = require('../models/user');
+const Event = require('../models/user.js');
+
+// Returns list of events in JSON for front end to display
+router.post('/getEvents', async (req, res) => {
+   try {
+        const { userId } = req.body;
+        const user = await User.findById(userId);
+        
+        if (!user) return res.status(204).json({ message: 'User not found' });
+
+        res.json(user.events);
+
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = router;
