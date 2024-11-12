aa3dee2e14767e2f8d015e8f8725852e512423cc
mattcattb <mboughton17@gmail.com>
2024-11-12 16:25:58 -0500

    Updated events meetings and users static database to be added to mongodb for testing.

--- wtm-express/src/server.js
+++ wtm-express/src/server.js
@@ -1,33 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
 const cors = require('cors');
 const connectdb = require('./repository/db');
 
 const auth = require('./routes/auth.routing');
 const meeting = require('./routes/meeting.routing');
-const events = require('./routes/events.routing')
+const events = require('./routes/events.routing');
 const friends = require('./routes/friends.routing');
 
+// Load environment variables
 dotenv.config();
 
 const app = express();
 app.use(cors()); // THIS IS SO UNSAFE PLEASE DO NOT LEAVE THIS HERE PERMANENTLY -Corey
 connectdb();
 
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON
 
+// Test Endpoint
 app.get('/', (req, res) => {
   res.send('Please work :3');
 });
 
+// Routes
 app.use('/api/auth', auth);
 app.use('/api/events', events);
 app.use('/api/friends', friends);
 app.use('/api/meeting', meeting);
 
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
