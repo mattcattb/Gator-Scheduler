f64dbc962d188d5266cf8fe5441f1af5f299d9c0
desanctis-n <desanctis.n@ufl.edu>
2024-11-15 17:41:17 -0500

    resolved platform compatibility issues with npm commands

--- wtm-express/src/repository/db.js
+++ wtm-express/src/repository/db.js
@@ -8,6 +8,7 @@ let mongoServer;
 const connectdb = async () => {
     try {
         let mongoURI;
+        console.log(process.env.NODE_ENV);
 
         if (process.env.NODE_ENV === 'test') {
             mongoServer = await MongoMemoryServer.create();
